b596635f20876bc71d50f7414ae9d1040e621d17
timi-codes <tejumoladavid@gmail.com>
2025-02-19 22:21:35 -0500

    v 1.0.13

--- dist/components/SelectWalletModal/hooks/useConnect.d.ts
+++ dist/components/SelectWalletModal/hooks/useConnect.d.ts
@@ -1,5 +1,7 @@
-import { Wallet } from '../../../providers/OrdConnectProvider';
+import { BitcoinNetworkType } from 'sats-connect';
+import { Network, Wallet } from '../../../providers/OrdConnectProvider';
 
+export declare const NETWORK_TO_BITCOIN_NETWORK_TYPE: Record<Network, BitcoinNetworkType>;
 export declare function useConnect({ onClose, onError: onUserError, }: {
     onClose: () => void;
     onError: (err: string) => void;
